446e069bb6e0644b484b31a97f6d664cc517eb3a
lishuailibertine <lishuai19@yeah.net>
2022-08-19 10:26:49 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(name:"Blake2",url: "https://github.com/tesseract-one/Blake2.swift.git", from: "0.1.2"),
         .package(url: "https://github.com/attaswift/BigInt", from: "5.3.0"),
         .package(name:"CBORSwift", url: "https://github.com/lishuailibertine/CBORSWift",from: "0.0.1"),
-        .package(url: "https://github.com/lishuailibertine/Base32", branch: "master")
+        .package(url: "https://github.com/lishuailibertine/Base32", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
